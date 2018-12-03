f170090f5e3102a7d95676221b5285f15c692ca6
johang08 <42912561+johang08@users.noreply.github.com>
2018-12-02 20:44:11 -0400

    Ready

---
+++ MrPaperTests/BLL/ComprasBLLTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MrPaper.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrPaper.BLL.Tests
+{
+    [TestClass()]
+    public class ComprasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
